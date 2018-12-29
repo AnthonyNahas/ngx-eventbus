8764075cc88efe54c52a5456ae00030be6148995
Anthony Nahas <anthony.na@hotmail.de>
2018-12-29 16:23:20 +0200

    chore(package): added source code of the library (pre-generated)

---
+++ src/module/component/lib.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Ngx-component',
+  templateUrl: './lib.component.html',
+  styleUrls: ['./lib.component.scss']
+})
+export class LibComponent {
+  description = 'ngx-eventbus is an angular open source library that simplifies the communication between angular directives and compon...';
+}
